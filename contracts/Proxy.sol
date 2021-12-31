b123041342ed12f274ca39c5381095923b416296
AskMik <64740164+AskMik@users.noreply.github.com>
2021-12-31 19:18:52 +0000

    updated

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -2,104 +2,54 @@
 pragma solidity >=0.8.10;
 
 
-contract Proxy{
-    
-    // Storage position of the address of the current logic code (Token1.sol)
-    bytes32 private constant logicAddress = 
-        keccak256("0xaE036c65C649172b43ef7156b009c6221B596B8b");
-    
-    // Storage position of the owner of the proxy contract
-    bytes32 private constant proxyOwnerPosition = 
-        keccak256("0x5B38Da6a701c568545dCfcB03FcB875f56beddC4");
-    
-    bytes32 testingVariable = proxyOwnerPosition;
-    
-    /**
-    * @dev Throws if called by any account other than the owner.
-    */
-    modifier onlyProxyOwner() {
-        require (msg.sender == proxyOwner());
-        _;
+pragma solidity ^0.8.6;
 
+contract Proxy {
+  bytes32 private constant _ADMIN_SLOT = 0x73b49fe9b2415f7043a9fabd65f4f4ea3962b31d51617906e5ba75ced62a3fb9;
+  bytes32 private constant _IMPLEMENTATION_SLOT = 0x42e0fb0da90f06f13d7593a53bf44cc09cbabf617547961fe2caac8f51d27773;
+ 
+  
+  constructor() {
+    bytes32 slot = _ADMIN_SLOT;
+    address _admin = msg.sender;
+    assembly {
+      sstore(slot, _admin)
+    }
+  }
 
+  function admin() public view returns (address adm) {
+    bytes32 slot = _ADMIN_SLOT;
+    assembly {
+      adm := sload(slot)
     }
-    
-    /**
-    * @dev the constructor sets owner
-    */
-    constructor() public {
-        _setUpgradeabilityOwner(msg.sender);
-    }
-    
-    /**
-     * @dev Allows the current owner to transfer ownership
-     * @param _newOwner The address to transfer ownership to
-     */
-    function transferProxyOwnership(address _newOwner) 
-        public onlyProxyOwner 
-    {
-        require(_newOwner != address(0));
-        _setUpgradeabilityOwner(_newOwner);
-    }
-    
-    /**
-     * @dev Allows the proxy owner to upgrade the implementation
-     * @param _implementation address of the new implementation
-     */
-    function upgradeTo(address _implementation) 
-        public onlyProxyOwner
-    {
-        _upgradeTo(_implementation);
-    }
-    
-    
-    function implementation() public view returns(address impl) {
-       bytes32 I_position = logicAddress;
-        assembly {
-            impl := sload(I_position)
-        }
-    }
-    
+  }
 
-    function proxyOwner() public view returns (address owner) {
-        bytes32 O_position = proxyOwnerPosition;
-        assembly {
-            owner := sload(O_position)
-        }
+  function implementation() public view returns (address impl) {
+    bytes32 slot = _IMPLEMENTATION_SLOT;
+    assembly {
+      impl := sload(slot)
     }
+  }
+
+  function upgrade(address newImplementation) external {
+    require(msg.sender == admin(), 'admin only');
+    bytes32 slot = _IMPLEMENTATION_SLOT;
+    assembly {
+      sstore(slot, newImplementation)
+    }
+  }
+
+  receive() external payable {
     
-    /**
-     * @dev Sets the address of the current implementation
-     * @param _newImplementation address of the new implementation
-     */
-    function _setImplementation(address _newImplementation) 
-        internal 
-    {
-        bytes32 position = logicAddress;
-        assembly {
-            sstore(position, _newImplementation)
-        }
+  }
+
+  fallback() external payable {
+    assembly {
+      let _target := sload(_IMPLEMENTATION_SLOT)
+      calldatacopy(0x0, 0x0, calldatasize())
+      let result := delegatecall(gas(), _target, 0x0, calldatasize(), 0x0, 0)
+      returndatacopy(0x0, 0x0, returndatasize())
+      switch result case 0 {revert(0, 0)} default {return (0, returndatasize())}
     }
-    
-    /**
-     * @dev Upgrades the implementation address
-     * @param _newImplementation address of the new implementation
-     */
-    function _upgradeTo(address _newImplementation) internal {
-        address currentImplementation = implementation();
-        require(currentImplementation != _newImplementation);
-        _setImplementation(_newImplementation);
-    }
-    
-    /**
-     * @dev Sets the address of the owner
-     */
-    function _setUpgradeabilityOwner(address _newProxyOwner) 
-        internal 
-    {
-        bytes32 position = proxyOwnerPosition;
-        assembly {
-            sstore(position, _newProxyOwner)
-        }
-    }
+  }
 }
